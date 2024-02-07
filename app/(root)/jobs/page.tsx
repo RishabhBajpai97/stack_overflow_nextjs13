0347a42be5087d2441051972d08ba89acd1fe10e
RishabhBajpai97 <bajpai.rishabh1018@gmail.com>
2024-02-07 23:35:37 +0530

    add ask-question and collection in publicRoutes

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Jobs</div>
+  )
+}
+
+export default Page
--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook", "question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs"],
+  publicRoutes: ["/", "/api/webhook", "question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs", "/ask-question", "/collection"],
   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
